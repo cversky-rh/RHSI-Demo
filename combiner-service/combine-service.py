5fb9c4725f72d9e21efa40c6c6559f6bc91f48cc
cversky-rh <61512772+cversky-rh@users.noreply.github.com>
2023-11-07 00:32:52 -0800

    Update combine-service.py

--- combiner-service/combine-service.py
+++ combiner-service/combine-service.py
@@ -10,46 +10,45 @@ urls = []
 try:
     with open(file_path, 'r') as file:
         for line in file:
-            ip = line.strip()
-            urls.append(ip)
-except FileNotFoundError:
-    print(f"File '{file_path}' not found.")
+            address = line.strip()
+            urls.append(address)
+except FileNotFoundError as e:
+    print(f"Error: {e}")
 
 print("IP addresses read from the file:", urls)
 
 app = Flask(__name__)
 CORS(app)
 
-#@app.route('/ads-b-combined')
 @app.route('/combined_get')
 def combined_get():
     report_time = 0
     elapsed_time = 0
-    states = []
-    all_states = ""
+    all_states = []
 
     for url in urls:
         try:
             response = requests.get(url)
-        except:
-            print("No data from " + url)
-        else:
-            data_json = json.loads(response.text)
+            response.raise_for_status()  # Check for HTTP errors
+            data_json = response.json()
             report_time = data_json['report_time']
             elapsed_time += data_json['elapsed_time_us']
-            states = str(data_json['states']).replace('[', '').replace(']', '').replace('\'', '\"')
-            all_states += states
-            print(states)
+            states = data_json['states']
+            all_states.extend(states)
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data from {url}: {e}")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON from {url}: {e}")
+        except KeyError as e:
+            print(f"Error processing data from {url}: Missing key {e}")
 
-    report_time = str(report_time)
-    elapsed_time = str(elapsed_time)
-    all_states = all_states.replace('}{', '},{')
-    print(all_states)
-    new_message = '{"report_time":' + report_time + ',"elapsed_time_us":' + elapsed_time + ',"states":[' + all_states + ']}'
-    new_data = json.loads(new_message)
+    new_data = {
+        "report_time": report_time,
+        "elapsed_time_us": elapsed_time,
+        "states": all_states
+    }
 
     return new_data
 
 if __name__ == '__main__':
     app.run()
-            
